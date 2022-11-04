d8820522851d95e4a9f9b88abb2db0757267db78
Bega-pixel <bega2000@protonmail.com>
2022-11-04 12:14:00 +0100

    ejer7.2

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const familiaNombres = new Set("Montana","Brett","Brady");
 familiaNombres.add("Joe");
 familiaNombres.add("Joe");
 familiaNombres.add("Javascript");
+
+
---
+++ objetos.js
@@ -0,0 +1,28 @@
+const persona = {
+        nombre:"Steve",
+        apellido:"Young",
+        edad: 25,
+        altura:"1.85 m",
+        eresDesarrolador:false
+
+    };
+
+const edad = persona.edad;
+
+const nuevaPersonas = [
+    {
+        ...datos
+    },{
+        nombre: "Jerry",
+        apellido: "Rice",
+        edad: 31,
+        altura: "188 m",
+        eresDesarrollador: false
+    },{
+        nombre: "John",
+        apellido: "Taylor",
+        edad: 34,
+        altura: "165 m",
+        eresDesarrollador: false
+    }
+]
